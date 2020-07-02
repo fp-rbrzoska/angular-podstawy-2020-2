7ed80839190e5886f4d0dde72ecf81d39a331507
Rafal Brzoska <rbrzoska@future-processing.com>
2020-07-02 12:02:38 +0200

    part1

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fp-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  homeTitle = 'My Home Page';
+  myNumbr = 1;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  formatCurrency(val: number) {
+    return 'EUR ' + val.toFixed(2)
+  }
+
+}
